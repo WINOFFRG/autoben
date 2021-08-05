8122222da1bf70f95247453bc7c4e66aedd0e259
WINOFFRG <rohangupta1528@gmail.com>
2021-08-06 02:51:31 +0530

    Session Login

--- core/browser.js
+++ core/browser.js
@@ -11,32 +11,40 @@ dotenv.config({path:'../config/config.env'});
     headless: false,
     // devtools: true
   });
+
+  // Define Chromium Properties
   const context = await browser.newContext({
     permissions: ['microphone'],
     colorScheme: 'dark',
     locale: 'en-US' ,
-    storageState: 'state.json'
+    storageState: '../config/state.json',
   });
 
   await context.route('**/*', route => route.continue());
-
   const page = await context.newPage();
+  // await page.setViewportSize({ width: null, height: null });
+  
   await page.goto('https://teams.microsoft.com');
   
+  // Check if login Required or Not
+  if(page.url().indexOf('https://login.microsoftonline.com/common/oauth2/authorize?') == 0) 
+  {
+      await page.fill('[placeholder="Email, phone, or Skype"]', process.env.EMAIL);
+      await page.click('text=Next');
+      await page.fill('[placeholder="Password"]', process.env.PASSWORD);
+      await page.click('text=Sign in');
 
-      // await page.fill('[placeholder="Email, phone, or Skype"]', process.env.EMAIL);
-      // await page.click('text=Next');
-      // await page.fill('[placeholder="Password"]', process.env.PASSWORD);
-      // await page.click('text=Sign in');
-
-      // const text = await page.innerText('.text-title');
-      // if(text === "Stay signed in?") await page.click('text=No');      
+      const text = await page.innerText('.text-title');
+      if(text === "Stay signed in?") await page.click('text=No');
+      
+      console.log("Signed In Successfully");
+  }
   
-  console.log("Signed In Successfully");
+  await page.waitForTimeout(10000);
 
-  await page.waitForTimeout(30000);
-
+  //Save session Data
   await context.storageState({ path: '../config/state.json' });
+  console.log("Saved Session Data");
 
   await page.pause();
 })();
