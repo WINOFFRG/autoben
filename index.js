39a2245d8ff974378a6afef95fabdb68f5fb154f
WINOFFRG <rohangupta1528@gmail.com>
2021-08-10 14:28:41 +0530

    Testing Web View

--- index.js
+++ index.js
@@ -1,41 +1,18 @@
-const fs = require('fs');
-const initBrowser = require('./core/browser');
-const filterTeams = require('./core/channels');
-const search = require('./core/searching');
+const { BrowserView, BrowserWindow, app } = require('electron')
 
-(async () => {
+function twoViews () {
+  const win = new BrowserWindow({ width: 800, height: 600 })
 
-    await initBrowser().then( (page) => {
-        readFile();
-        search(page);
+  const view = new BrowserView({ useContentSize: true })
+  win.addBrowserView(view)
+  view.setBounds({ x: 0, y: 0, width: 800, height: 800 })
+  view.setAutoResize({width:true , height:false});
+  view.webContents.loadURL('https://google.com')
 
-    });
+  app.on('window-all-closed', () => {
+    win.removeBrowserView(view)
+    app.quit()
+  })
+}
 
-    function readFile(){
-        
-        fs.readFile('./config/capture-api-response.json','utf-8', async function(err, jsonData){
-            if (err) throw err;    
-            var content = JSON.stringify(jsonData);
-            var data = JSON.parse(content);
-            // return await data;
-
-            printFilteredTeams(data);
-        });
-    }
-    
-    async function printFilteredTeams(data){
-        
-        filterTeams(data).then( (resp) => {
-            fs.writeFile('./config/filtered-teams.json', resp, (err) => {
-                if (err) throw err;
-                console.log('The file has been saved!');
-              });
-            console.log('handle success here');
-            console.log('Starting Searching');
-         }).catch((e) => {
-            console.log('handle error here: ', e.message)
-         })
-    }
-
-    readFile();
-})();
+app.whenReady().then(twoViews)
